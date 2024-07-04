753fe27dfdad8f3b6e0a087bb3c3781ac5f0f477
CristopherBuitrago <cristopherbuitrago0907@gmail.com>
2024-07-04 15:56:04 -0500

    modificados:     betplay/src/main/java/com/ligabetplay/model/entity/User.java 	modificados:     betplay/src/main/java/com/ligabetplay/model/entity/UserDAO.java 	modificados:     betplay/src/main/resources/db.properties 	renombrados:     betplay/src/main/resources/ddl.sql -> betplay/src/main/resources/db/commands/ddl.sql 	renombrados:     betplay/src/main/resources/dml.sql -> betplay/src/main/resources/db/commands/dml.sql 	renombrados:     betplay/src/main/resources/ligabetplay.mwb -> betplay/src/main/resources/db/diagram/ligabetplay.mwb 	renombrados:     betplay/src/main/resources/ligabetplay.mwb.bak -> betplay/src/main/resources/db/diagram/ligabetplay.mwb.bak 	modificados:     betplay/target/classes/com/ligabetplay/model/entity/User.class 	modificados:     betplay/target/classes/com/ligabetplay/model/entity/UserDAO.class 	modificados:     betplay/target/classes/db.properties 	renombrados:     betplay/target/classes/ddl.sql -> betplay/target/classes/db/commands/ddl.sql 	renombrados:     betplay/target/classes/dml.sql -> betplay/target/classes/db/commands/dml.sql 	renombrados:     betplay/target/classes/ligabetplay.mwb -> betplay/target/classes/db/diagram/ligabetplay.mwb 	renombrados:     betplay/target/classes/ligabetplay.mwb.bak -> betplay/target/classes/db/diagram/ligabetplay.mwb.bak

--- betplay/src/main/java/com/ligabetplay/model/entity/User.java
+++ betplay/src/main/java/com/ligabetplay/model/entity/User.java
@@ -34,7 +34,7 @@ public class User {
         return username;
     }
 
-    public void setName(String username) {
+    public void setUsername(String username) {
         this.username = username;
     }
 
