e9b144858ef144e7ff5ce87fb65c8cdf9ae9e68e
CristopherBuitrago <cristopherbuitrago0907@gmail.com>
2024-07-05 23:10:09 -0500

    Modificado SponsorUpdateDAOImpl

--- betplay/src/main/java/com/ligabetplay/model/sponsor/SponsorUpdateDAOImpl.java
+++ betplay/src/main/java/com/ligabetplay/model/sponsor/SponsorUpdateDAOImpl.java
@@ -1,18 +1,19 @@
 package com.ligabetplay.model.sponsor;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
-public class SponsorUpdateDAOImpl implements ISponsorUpdateDAO {
+public class SponsorUpdateDAOImpl  {
   private final Connection connection;
 
   public SponsorUpdateDAOImpl(Connection connection) {
     this.connection = connection;
   }
-
+/* 
   @Override
   public void updateSponsor(Sponsor sponsor);
+}*/
 }
-
 
 /*
  * Completar punto 11 de patrocinos"
