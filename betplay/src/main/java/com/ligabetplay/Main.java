6c4f943947e4c5da395b5fdf71534de3e6230b0c
CristopherBuitrago <cristopherbuitrago0907@gmail.com>
2024-07-05 23:16:16 -0500

    modified:   betplay/src/main/java/com/ligabetplay/Main.java 	modified:   betplay/target/classes/com/ligabetplay/Main.class

--- betplay/src/main/java/com/ligabetplay/Main.java
+++ betplay/src/main/java/com/ligabetplay/Main.java
@@ -9,6 +9,9 @@ import com.ligabetplay.view.user.UserView;
 
 public class Main {
     public static void main(String[] args) {
+        /*
+         * make sure to create the database in Resources folder -> db -> commands -> ddl.sql
+         */
         // loggin
         IUserView view = new UserView();
         IUserService service = new UserService();
